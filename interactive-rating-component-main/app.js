7dc26935758414876412f66a958a9c67e191f8b3
birkaany <birkanyilmaz41@gmail.com>
2022-09-07 01:36:50 +0300

    rate results designed and activated

--- interactive-rating-component-main/app.js
+++ interactive-rating-component-main/app.js
@@ -1,4 +1,11 @@
 const rates = document.querySelectorAll(".rate");
+const submitBtn = document.querySelector(".submitBtn");
+const ratesSection = document.querySelector(".rates");
+const heading = document.querySelector(".text-block h1");
+const paraText = document.querySelector(".text-block p");
+const imgTop = document.querySelector("picture img");
+const mainDiv = document.querySelector(".rating-component");
+const rateResult = document.querySelector(".rate-result");
 
 rates.forEach(function (rate) {
   rate.addEventListener("click", function () {
@@ -8,6 +15,25 @@ rates.forEach(function (rate) {
       }
     }
     rate.classList.add("active");
-    console.log(rate.innerHTML);
+    rateResult.innerHTML = `You selected ${rate.textContent} out of 5`;
+    return rate;
   });
+});
+
+function showResults(rate) {
+  heading.innerHTML = "Thank you!";
+  paraText.innerHTML =
+    "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+  ratesSection.classList.add("hidden");
+  submitBtn.classList.add("hidden");
+  imgTop.src = "/images/illustration-thank-you.svg";
+  imgTop.parentElement.classList.remove("bg-circle");
+  mainDiv.style.alignItems = "center";
+  mainDiv.style.textAlign = "center";
+  mainDiv.style.justifyContent = "space-evenly";
+  rateResult.classList.remove("hidden");
+}
+
+submitBtn.addEventListener("click", function () {
+  showResults();
 });
