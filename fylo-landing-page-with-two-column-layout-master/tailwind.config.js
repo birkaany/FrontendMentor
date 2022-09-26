9364ff9dddd96e5ff86a6ca8171d0a39cd17cc63
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-09-27 02:08:01 +0300

    mobile designed

--- fylo-landing-page-with-two-column-layout-master/tailwind.config.js
+++ fylo-landing-page-with-two-column-layout-master/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      openSans: ["Open Sans", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        bgCurveMobile: "url('images/bg-curve-mobile.svg')",
+        bgCurveDesktop: "url('images/bg-curve-desktop.svg')",
+      },
+      colors: {
+        veryDarkblue: "hsl(243, 87%, 12%)",
+        dsBlue: "hsl(238, 22%, 44%)",
+        brightblue: "hsl(224, 93%, 58%)",
+        dsCyan: "hsl(170, 45%, 43%)",
+        lightBlue: "hsl(240, 75%, 98%)",
+        lightGray: "hsl(0, 0%, 75%)",
+      },
+    },
   },
   plugins: [],
 };
