3c26237e45df28baeedf094432cbecb125ee3a9b
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-10-01 03:23:59 +0300

    mobile designed

--- order-summary-component-main/tailwind.config.js
+++ order-summary-component-main/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      fontRedHat: ["Red Hat Display", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        paleBlue: "hsl(225, 100%, 94%)",
+        brightBlue: "hsl(245, 75%, 52%)",
+        veryPaleblue: "hsl(225, 100%, 98%)",
+        dsBlue: "hsl(224, 23%, 55%)",
+        darkBlue: "hsl(223, 47%, 23%)",
+      },
+    },
   },
   plugins: [],
 };
